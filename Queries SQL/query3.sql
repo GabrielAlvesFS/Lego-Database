f769afcc4c60a4656efc6519cf11bec084b7a857
Bruna Xavier <brunaxavier@id.uff.br>
2022-07-12 14:26:32 -0300

    :bug: fix: complemento da query 4; adição de queries do supabase;

--- Queries SQL/query3.sql
+++ Queries SQL/query3.sql
@@ -1,5 +1,15 @@
 -- 3 - Quais são os 10 temas mais populares (mais feitos) de lego?
+
+-- QUERY MYSQL:
+
 SELECT sets.theme_id, COUNT(sets.theme_id) as tema_repetido FROM sets
   GROUP BY sets.theme_id
+  ORDER BY tema_repetido DESC
+  LIMIT 10;
+  
+  -- QUERY SUPABASE:
+  
+  SELECT theme_id, COUNT(theme_id) as tema_repetido FROM "LEGO_sets"
+  GROUP BY theme_id
   ORDER BY tema_repetido DESC
   LIMIT 10;

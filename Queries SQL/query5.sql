c06cdd7dcd352098d9b270ccdbfa0dc9a935cb8b
luanaassisi <luanaaraujodeassis@outlook.com>
2022-07-12 19:58:07 -0300

    query para o supabase

--- Queries SQL/query5.sql
+++ Queries SQL/query5.sql
@@ -1,13 +1,15 @@
 #Quais os anos com maiores produções de "Star Wars"
+# Query para o Supabase
 
 SELECT 
-    sets.year, COUNT(themes.name) AS prod_star_wars
-FROM
-    sets
-        INNER JOIN
-    themes ON sets.theme_id = themes.id
+    "LEGO_sets".year, count("LEGO_themes".name) AS prod_star_wars
+FROM "LEGO_sets"
+INNER JOIN 
+    "LEGO_themes" ON "LEGO_sets".theme_id = "LEGO_themes".id
 WHERE
-    themes.name = 'Star Wars'
-ORDER BY prod_star_wars desc
+    "LEGO_themes".name = 'Star Wars'
+GROUP BY "LEGO_sets".year
+ORDER BY prod_star_wars desc;
+
 
 
